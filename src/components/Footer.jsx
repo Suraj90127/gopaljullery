cb3f935dfb718436cd5028650c825647be0d54f6
juli-kumari16 <150012861+juli-kumari16@users.noreply.github.com>
2024-10-21 17:16:22 +0530

    add footer fron juli

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <h1>This is Footer components</h1>
+      <h1>This is Footer components llllllllllllloiueierieri</h1>
     </div>
   );
 };
